66432852f61865f07998a1eedbce6a0710374c1c
Shang-Hung Tsai <shtsai7@gmail.com>
2018-02-18 14:45:03 -0500

    update task 1

--- assignment1/task1/map.py
+++ assignment1/task1/map.py
@@ -6,16 +6,6 @@ import sys
 import string
 #import numpy
 
-'''
-    Try parse input string.
-'''
-def tryParseInt(input):
-    try:
-        intValue = int(input)
-        return True, intValue
-    except ValueError:
-        return False, -1
-
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
@@ -27,30 +17,26 @@ for line in sys.stdin:
     entry = line.split(",")
 
     # If this is an entry from parking-violations.csv
-    #if (len(entry) == 22):
-    if "parking" in os.environ.get("mapreduce_map_input_file"):
-        summonsStatus, summonsNumber = tryParseInt(entry[0])
+    if (len(entry) == 22):
+    #if "parking" in os.environ.get("mapreduce_map_input_file"):
+        summonsNumber = entry[0]
         plateId = entry[14]
-        violationPrecinctStatus, violationPrecinct = tryParseInt(entry[6])
-        violationCodeStatus, violationCode = tryParseInt(entry[2])
+        violationPrecinct = entry[6]
+        violationCode = entry[2]
         issueDate = entry[1]
         status = "All"
 
-        # Check if all fields are parsed successfully
-        if not (summonsStatus and violationPrecinctStatus and violationCodeStatus):
-            continue
 
-        print('{0:d}\t{1:s}, {2:s}, {3:d}, {4:d}, {5:s}'.format(summonsNumber, status, plateId, violationPrecinct, violationCode, issueDate))
+        print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}, {5:s}'.format(summonsNumber, status, plateId, violationPrecinct, violationCode, issueDate))
 
 
     # Otherwise, if this is an entry from open-violations.csv
-    #else:
-    elif "open" in os.environ.get("mapreduce_map_input_file"):
-        summonsStatus, summonsNumber = tryParseInt(entry[0])
+    else:
+    #elif "open" in os.environ.get("mapreduce_map_input_file"):
+        summonsNumber = entry[0]
         status = "Open"
 
-        if not summonsStatus:
-            continue
 
-        print('{0:d}\t{1:s}, {2:s}, {3:d}, {4:d}, {5:s}'.format(summonsNumber, status, "tmp", -1, -1, "tmp"))
+        print('{0:s}\t{1:s}, {2:s}, {3:s}, {4:s}, {5:s}'.format(summonsNumber, status, "x", "x", "x", "x"))
+
 
