6ad29ae6e2fe3a5223da3864d907150633fcfc60
Shang-Hung Tsai <shtsai7@gmail.com>
2018-02-18 14:46:51 -0500

    update task 2

--- assignment1/task2/map.py
+++ assignment1/task2/map.py
@@ -5,24 +5,13 @@ import sys
 import string
 #import numpy
 
-'''
-    Try parse input string.
-'''
-def tryParseInt(input):
-    try:
-        intValue = int(input)
-        return True, intValue
-    except ValueError:
-        return False, -1
-
 
 # input comes from STDIN (stream data that goes to the program)
 for line in sys.stdin:
     line = line.strip()
     entry = line.split(",")
 
-    violationCodeStatus, violationCode = tryParseInt(entry[2])
-    if violationCodeStatus:
-        print('{0:d}\t{1:d}'.format(violationCode, 1))
+    violationCode = entry[2]
+    print('{0:s}\t{1:d}'.format(violationCode, 1))
 
 
