ea77c0f89c36c315c57e4681791f5ca4588843d1
Shang-Hung Tsai <shtsai7@gmail.com>
2018-02-22 22:18:38 -0500

    add csv reader to task3/map.py

--- assignment1/task3/map.py
+++ assignment1/task3/map.py
@@ -3,13 +3,13 @@
 
 import sys
 import string
+import csv
 #import numpy
 
 # input comes from STDIN (stream data that goes to the program)
-for line in sys.stdin:
-    line = line.strip()
-    entry = line.split(",")
 
+data = csv.reader(sys.stdin)
+for entry in data:
     licenceType = entry[2]
     amount = entry[13]
     print('{0:s}\t{1:s}'.format(licenceType, amount))
